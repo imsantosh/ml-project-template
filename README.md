bba19fb62e4bef47c35829aa07269f637014afcc
imsantosh <santosh76792@gmail.com>
2024-06-30 13:07:30 +0200

    Updated readme file with steps and explanation for use of different python file

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-This repository can be used as ML/DL project structure or template creation for enhance modularity and code management.
+This repository can be used to create a modular and well-managed structure for ML/DL projects. Here's a step-by-step guide to setting up and using this template:
 
 ## Steps:
 1) clone this repo.
 2) Create a virtual enviroment
-3) Run 'python template.py'. This is create the all required files and folders as part of this template. review the template.py to see/update(add or remove) the project structure.
+- Create virtual enviroment: python -m venv tutorial-env
+- Activate virtual enviroment: 
+  - On Windows, run: tutorial-env\Scripts\activate
+  - On Unix or MacOS, run: source tutorial-env/bin/activate    
+3) Run 'python template.py'. This script will create all required files and folders as part of the template. You can review and update 'template.py' to customize the project structure according to your needs.
 4) Update the setup.py file with resepect to project name and your email id & name.
-5) Review the requirements.txt file to add or remove python libraries.
-6) Install requirements.txt file to install the mentioned library and to enable project as package.
+5) Add or remove Python libraries as needed. Ensure the last line of the file includes '-e .' to enable the project as a package.
+6) Install requirements.txt file by running 'pip install -r requirements.txt'. This command will install the required libraries and enable your project as a package using setup.py
+
+## Use of different files:
+# 'Template.py':
+- This script is used to create the initial project structure and files. Modify it to fit the specific needs of your project.
+# Setup.py:
+- This file is used to enable your project as a package. It includes metadata about your project and its dependencies.
+# Requirements.txt:
+- This file lists the libraries required for your project. By including '-e .' at the end, While running the 'pip install -r requirements.txt' will also look for setup.py and will enable your project as a package.
+
+# Note:- 
+- This script will set up a basic folder structure for your project. You can expand it based on your requirements.
+- By following these steps, you can ensure a modular and well-organized ML/DL project, making it easier to manage and scale your code.
--- template.py
+++ template.py
@@ -1,30 +1,30 @@
 import os
 from pathlib import Path
 
-project_name = "project_name"
+PROJECT_NAME = "project_name"
 
 list_of_files = [
 
-    f"{project_name}/__init__.py",
-    f"{project_name}/components/__init__.py",
-    f"{project_name}/components/data_ingestion.py",  
-    f"{project_name}/components/data_validation.py",
-    f"{project_name}/components/data_transformation.py",
-    f"{project_name}/components/model_trainer.py",
-    f"{project_name}/components/model_evaluation.py",
-    f"{project_name}/components/model_pusher.py",
-    f"{project_name}/configuration/__init__.py",
-    f"{project_name}/constants/__init__.py",
-    f"{project_name}/entity/__init__.py",
-    f"{project_name}/entity/config_entity.py",
-    f"{project_name}/entity/artifact_entity.py",
-    f"{project_name}/exception/__init__.py",
-    f"{project_name}/logger/__init__.py",
-    f"{project_name}/pipline/__init__.py",
-    f"{project_name}/pipline/training_pipeline.py",
-    f"{project_name}/pipline/prediction_pipeline.py",
-    f"{project_name}/utils/__init__.py",
-    f"{project_name}/utils/main_utils.py",
+    f"{PROJECT_NAME}/__init__.py",
+    f"{PROJECT_NAME}/components/__init__.py",
+    f"{PROJECT_NAME}/components/data_ingestion.py",  
+    f"{PROJECT_NAME}/components/data_validation.py",
+    f"{PROJECT_NAME}/components/data_transformation.py",
+    f"{PROJECT_NAME}/components/model_trainer.py",
+    f"{PROJECT_NAME}/components/model_evaluation.py",
+    f"{PROJECT_NAME}/components/model_pusher.py",
+    f"{PROJECT_NAME}/configuration/__init__.py",
+    f"{PROJECT_NAME}/constants/__init__.py",
+    f"{PROJECT_NAME}/entity/__init__.py",
+    f"{PROJECT_NAME}/entity/config_entity.py",
+    f"{PROJECT_NAME}/entity/artifact_entity.py",
+    f"{PROJECT_NAME}/exception/__init__.py",
+    f"{PROJECT_NAME}/logger/__init__.py",
+    f"{PROJECT_NAME}/pipline/__init__.py",
+    f"{PROJECT_NAME}/pipline/training_pipeline.py",
+    f"{PROJECT_NAME}/pipline/prediction_pipeline.py",
+    f"{PROJECT_NAME}/utils/__init__.py",
+    f"{PROJECT_NAME}/utils/main_utils.py",
     "app.py",
     "requirements.txt",
     "Dockerfile",
@@ -45,4 +45,4 @@ for filepath in list_of_files:
         with open(filepath, "w") as f:
             pass
     else:
-        print(f"file is already present at: {filepath}")
+        print(f"file is already present at: {filepath}\n")
