ec63897c37b139878c3d096965ff7730481c32c7
imsantosh <santosh76792@gmail.com>
2024-06-30 12:11:35 +0200

    add requirement, setup and template file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="project_name",
+    version="0.0.0",
+    author="Santosh",
+    author_email="<email-id>",
+    packages=find_packages()
+)
